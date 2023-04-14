f5b522b96e2101f1f2cc5a6679cd98e041f354d6
Julien Bertram <bertramjulien@gmail.com>
2023-04-14 16:12:53 -0400

    seed created

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "phases#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :phases
+  resources :phases do
+    resources :measurements
+  end
   # Defines the root path route ("/")
   # root "articles#index"
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Phase.destroy_all
+
+phase = Phase.create!(
+  user_id: 6,
+  phase_type: 'perte de poids',
+  started_date: Date.today,
+  period: 90
+)
+
+measure1 = Measurement.create!(
+  phase_id: phase.id,
+  date: '2023-03-20',
+  weight: 84,
+  bras: 37,
+  poitrine: 105,
+  taille: 89,
+  hanches: 97,
+  cuisses: 64
+)
+
+measure2 = Measurement.create!(
+  phase_id: phase.id,
+  date: '2023-04-03',
+  weight: 83,
+  bras: 37.5,
+  poitrine: 101,
+  taille: 88,
+  hanches: 97,
+  cuisses: 65
+)
